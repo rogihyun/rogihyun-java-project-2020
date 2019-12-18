4bf9abb0f29849dd9953f8a0c552ee7b79bb7e19
rogihyun <srlgus2002v@naver.com>
2019-12-18 16:03:36 +0900

    프로젝트 초기화

---
+++ src/main/java/rogihyun/fashionmall/App.java
@@ -0,0 +1,9 @@
+// 프로젝트 : 패션쇼핑몰 자재관리
+//
+package rogihyun.fashionmall;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("패션쇼핑몰에 오신걸 환영합니다.");
+    }
+}

aa222503498ebf088d6e22c3b19e01f0572c136c
rogihyun <srlgus2002v@naver.com>
2019-12-18 16:35:12 +0900

    222출력삭제

--- src/main/java/rogihyun/fashionmall/App.java
+++ src/main/java/rogihyun/fashionmall/App.java
@@ -6,7 +6,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("패션쇼핑몰에 오신걸 환영합니다.");
         System.out.println("111");
-        System.out.println("222");
+       
        
     }
 }
